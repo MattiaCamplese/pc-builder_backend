8778aa234650c25877654d2aa2c940ea1b37f751
Mattiacamplese <mattia1996@gmail.com>
2026-06-17 15:44:44 +0200

    push

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import authRoute from './routes/auth.routes.js'
 import userRoute from './routes/user.route.js'
 import componentsRoute from './routes/components.route.js'
 import buildsRoute from './routes/builds.route.js'
 
 const app = new Hono()
+
+app.use('/api/*', cors({
+  origin: '*',
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+}))
 
 app.route('/api', authRoute)
 app.route('/api', userRoute)
